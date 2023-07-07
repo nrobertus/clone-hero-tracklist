26b646f6301804ded73bd72befe20b4d340e9d5a
nrobertus <nathan.robertus@gmail.com>
2023-07-07 12:58:56 -0600

    Added fuzzy sorting

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,34 +1,47 @@
 <script setup lang="ts">
 import json from "../data/songs.json";
-import Table from './Table.vue';
+import Table from "./Table.vue";
 import { defineComponent, reactive, ref, watch } from "vue";
 import { NInput } from "naive-ui";
 
-function sortMultiple(data: { Name: string, Artist: string }[]){
-  return data.sort((a, b)=> {
-  if (a.Artist === b.Artist){
-    return a.Name.localeCompare(b.Name)
-  } else {
-    return a.Artist.localeCompare(b.Artist)
-  }
-})
+function sortMultiple(data: { Name: string; Artist: string }[]) {
+  return data.sort((a, b) => {
+    if (a.Artist === b.Artist) {
+      return a.Name.localeCompare(b.Name);
+    } else {
+      return a.Artist.localeCompare(b.Artist);
+    }
+  });
 }
 
 const inputValue = ref("");
 let tableData = reactive(sortMultiple(json));
 watch(inputValue, (e) => {
+  const terms = e.split(" ").filter((n) => n);
+  console.log(terms);
+
   tableData = [];
-  json.forEach(song => {
-    if(song.Artist.toLowerCase().includes(e.toLowerCase()) || song.Name.toLowerCase().includes(e.toLowerCase())){
+  json.forEach((song) => {
+    let matches = 0;;
+    terms.forEach((term) => {
+      if (
+        song.Artist.toLowerCase().includes(term.toLowerCase()) ||
+        song.Name.toLowerCase().includes(term.toLowerCase())
+      ) {
+        matches += 1;
+      }
+    });
+
+    if (matches === terms.length) {
       tableData.push(song);
     }
-  })
+  });
 });
 </script>
 
 <template>
   <n-input v-model:value="inputValue"></n-input>
-  <Table :data=tableData />
+  <Table :data="tableData" />
 </template>
 
 <style scoped></style>
